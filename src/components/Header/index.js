da281b2b07faffb52a85d7a06486e983c6185589
Gustavo Valenga de Campos <88239833+vdc-gustavo@users.noreply.github.com>
2024-08-04 16:56:17 -0300

    Creating Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,25 @@
+import logo from "../../svg/logo.svg"
+
 const Header = () => {
-    return (
-        <h1>Header.js</h1>
+    return (<>
+        <header className="flex-space-between">
+            <div className="logo">
+            <img src={logo} alt="logo blog"/>
+            </div>
+            <div className="search">
+                <input type="text" className="input-search" name="search"></input>
+            </div>
+            <div>
+                <ul className="menu">
+                    <li><a href="#" className="nav-link">Categorias</a></li>
+                    <li><a href="#"  className="nav-link">Sobre</a></li>
+                    <li><a href="#"  className="nav-link">Contato</a></li>
+                </ul>
+            </div>
+        </header>
+        </>
     )
 }
 
 export default Header;
+
