bed9c10f0a31946bc5e8f23cea3c140a159a17b2
Gustavo Valenga de Campos <88239833+vdc-gustavo@users.noreply.github.com>
2024-08-05 18:56:40 -0300

    Front-end finished

--- src/components/Article/index.js
+++ src/components/Article/index.js
@@ -1,11 +1,50 @@
-import Post from "../Post"
+import Post from "../Post";
 
 const Article = () => {
   return (
-  <>
-  <h1>Article.js</h1>
-  <Post></Post>
-  </>
+    <>
+      <section className="container">
+        <h3>Artigos</h3>
+        <div className="container-posts mt-5">
+          <Post categoria="tecnologia" titulo="O guia definitivo do Blog">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+            eget malesuada odio, a blandit nisl. In maximus rutrum tellus, sit
+            amet elementum ante pretium id. Maecenas faucibus viverra tellus, et
+            ultricies leo iaculis fringilla. In massa ligula, tincidunt quis
+            lobortis vel, fermentum quis ante. Pellentesque erat ante,
+            consectetur in augue eu, aliquam elementum ex. Cras lectus quam,
+            sagittis et pretium tempor, rhoncus vel mauris. Aliquam ut justo
+            arcu.
+          </Post>
+          <Post
+            categoria="fotografia"
+            titulo="Quais os melhores celulares em 2023?"
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+            eget malesuada odio, a blandit nisl. In maximus rutrum tellus, sit
+            amet elementum ante pretium id. Maecenas faucibus viverra tellus, et
+            ultricies leo iaculis fringilla. In massa ligula, tincidunt quis
+            lobortis vel, fermentum quis ante. Pellentesque erat ante,
+            consectetur in augue eu, aliquam elementum ex. Cras lectus quam,
+            sagittis et pretium tempor, rhoncus vel mauris. Aliquam ut justo
+            arcu.
+          </Post>
+          <Post
+            categoria="cinema"
+            titulo="Os 10 filmes com maior bilheteria de 2023"
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+            eget malesuada odio, a blandit nisl. In maximus rutrum tellus, sit
+            amet elementum ante pretium id. Maecenas faucibus viverra tellus, et
+            ultricies leo iaculis fringilla. In massa ligula, tincidunt quis
+            lobortis vel, fermentum quis ante. Pellentesque erat ante,
+            consectetur in augue eu, aliquam elementum ex. Cras lectus quam,
+            sagittis et pretium tempor, rhoncus vel mauris. Aliquam ut justo
+            arcu.
+          </Post>
+        </div>
+      </section>
+    </>
   );
 };
 
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,9 +1,22 @@
-const Post = () => {
-    return(
-        <>
-        <h1>Post.js</h1>
-        </>
-    );
+const Post = (props) => {
+  return (
+    <div className="post mx-2">
+      <div className="img-post">
+        <h5>{props.categoria}</h5>
+        <h3 className="mb-2">{props.titulo}</h3>
+        <p>
+            {props.children}
+        </p>
+      </div>
+      <div className="flex pt-2">
+        <div className="img-profile"> </div>
+        <div className="desc-profile ml-2">
+          <h6 className="color-blue">guttta</h6>
+          <p> 04/08/2024 - 8 min de leitura</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Post;
